f92fb245e472888411ef8c006ab8a396ebd362c5
Rain999 <1163799833@qq.comgit>
2020-10-23 19:51:43 +0800

    添加富文本框

---
+++ src/main/java/com/example/demo/controller/FileCon.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.FileDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Objects;
+
+@Controller
+public class FileCon {
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDto upload(){
+
+
+        FileDto fileDto = new FileDto();
+        fileDto.setSuccess(1);
+        fileDto.setUrl("/img/11.jpg");
+
+
+        return fileDto;
+    }
+
+
+
+}
